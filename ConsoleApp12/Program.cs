95ccd54f2562a967c8e394f376e03bd44afbbb30
Orxan <ibrahimzada003@gmail.com>
2023-10-29 14:30:32 +0400

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,72 @@
+﻿#region task1
+//Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapın.
+//int num1 = 45;
+//bool Number(int num1)
+//{
+//    return num1 % 21 == 0 ? true : false;
+//}
+//Console.WriteLine(Number(num1));
+#endregion
+
+#region task2
+
+//int n = int.Parse(Console.ReadLine());
+//int m = int.Parse(Console.ReadLine());
+//string Them(int num1, int num2)
+//{
+
+//    if (num1 % 2 == 0 && num2 % 2 == 0)
+//    {
+//        int result = num1 + num2;
+//        return ($"bu ededin cemi {result} ");
+//    }
+//    return "cut deyil";
+//}
+//Console.WriteLine(Them(n, m));
+
+#endregion
+
+#region task3
+//int num1 = 8;
+//int num2 = 20;
+
+//string sum(int num1, int num2)
+//{
+//    int count = 0;
+//    for (int i = num1; i <= num2; i++)
+//    {
+//        if (num1 < num2)
+//        {
+//            if (i % 2 == 1)
+//            {
+//                count++;
+//            }
+//        }
+//    }
+//    return ($"arasindaki reqem sayi{count}");
+
+//}
+//Console.WriteLine(sum(num1, num2));
+#endregion
+
+#region task4
+//string sum(int n, int m)
+//{
+//    int sum = default(int);
+//    if (m > n)
+//    {
+//        for (int i = n; i <= m; i++)
+//        {
+//            if (i % 2 == 1)
+//            {
+//               sum += i;
+//            }
+//        }
+//        return $"{n} ve {m} ededleri arasindaki tek ededlerin cemi {oddSum} olur";
+//    }
+//    return $"1-ci eded 2-ci ededden kicikdir";
+//}
+//string sumResult = sum(10, 15);
+//Console.WriteLine(sumResult);
+#endregion
+
